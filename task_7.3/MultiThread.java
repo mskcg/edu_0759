e5f0dfb000a9b7273dd8bb5a8ea70688deaa565f
Kusemin <kuzemin@mail.ru>
2021-06-17 00:19:09 +0300

    многопоточный ProxyChecker 3м способом

---
+++ task_7.3/MultiThread.java
@@ -0,0 +1,77 @@
+// Реализовать многопоточность 2 и 3 способом
+
+public class MultiThread {
+    public static void main(String[] args) {
+
+        // Третий способ
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println("Мыть посуду выполнено: "+i+"%");
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println("Стирать белье выполнено: "+i+"%");
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+
+        /*
+        ** Второй способ
+        Thread myRunnableThread1 = new Thread(new MyRunnableThread("Мыть посуду"));
+        Thread myRunnableThread2 = new Thread(new MyRunnableThread("Стирать белье"));
+        Thread myRunnableThread3 = new Thread(new MyRunnableThread("Мыть полы"));
+        myRunnableThread1.start();
+        myRunnableThread3.start();
+        myRunnableThread2.start();*/
+
+        //** Первый способ
+
+        MyThread myThread1 = new MyThread("Мыть посуду");
+        MyThread myThread2 = new MyThread("Стирать белье");
+        MyThread myThread3 = new MyThread("Мыть полы");
+        myThread1.start();
+        myThread2.start();
+        myThread3.start();
+    }
+}
+
+class MyRunnableThread implements Runnable{
+    private String task;
+
+    public MyRunnableThread(String task) {
+        this.task = task;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(this.task+" выполнено: "+i+"%");
+        }
+    }
+}
+
+
+
+class MyThread extends Thread{
+    private String task;
+
+    public MyThread(String task) {
+        super();
+        this.task = task;
+    }
+
+    @Override
+    public void run(){
+        for (int i = 0; i < 100; i++) {
+            System.out.println(this.task+" выполнено: "+i+"%");
+        }
+    }
+}
